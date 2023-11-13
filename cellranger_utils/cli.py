750c39f927c95abd8bc3716cd6408b73de5fb474
Diljot <diljot.grewal@gmail.com>
2023-11-13 07:27:03 -0800

    refactor: added commands to run non multiplexed samples (#1)

--- cellranger_utils/cli.py
+++ cellranger_utils/cli.py
@@ -1,7 +1,11 @@
 """Console script for cellranger utils"""
-
+import yaml
 import click
-import cellranger_utils.utils as utils
+from cellranger_utils.cellranger_demultiplex import run_cellranger_demultiplex
+from cellranger_utils.cellranger_non_multiplexed import run_cellranger_non_multiplexed
+from cellranger_utils.cellranger_persample import run_cellranger_persample
+
+from cellranger_utils.bamtofastq import run_bam_to_fastq
 
 
 @click.group()
@@ -11,38 +15,96 @@ def cli():
 
 @cli.command()
 @click.option('--reference', required=True, help='CSV file path')
-@click.option('--meta_yaml', required=True, help='memory for cellranger multi')
+@click.option('--vdj_reference', help='CSV file path')
 @click.option('--gex_fastq', required=True, help='cores for cellranger multi')
 @click.option('--gex_id', required=True, help='cores for cellranger multi')
+@click.option('--outdir', required=True, help='cores for cellranger multi')
+@click.option('--meta_yaml', required=True, help='memory for cellranger multi')
+@click.option('--tempdir', required=True, help='cores for cellranger multi')
+@click.option('--sample_id', required=True, help='cores for cellranger multi')
+@click.option('--tcr_fastq', help='cores for cellranger multi')
+@click.option('--tcr_id', help='cores for cellranger multi')
 @click.option('--cite_fastq', help='cores for cellranger multi')
 @click.option('--cite_id', help='cores for cellranger multi')
-@click.option('--outdir', required=True, help='cores for cellranger multi')
-@click.option('--tempdir', required=True, help='cores for cellranger multi')
-@click.option('--numcores', required=True, help='cores for cellranger multi')
-@click.option('--mempercore', required=True, help='cores for cellranger multi')
-def cellranger_multi(
+@click.option('--bcr_fastq', help='cores for cellranger multi')
+@click.option('--bcr_id', help='cores for cellranger multi')
+@click.option('--numcores', default=16, help='cores for cellranger multi')
+@click.option('--mempercore', default=10, help='cores for cellranger multi')
+def cellranger_nonmultiplexed(
         reference,
-        meta_yaml,
+        vdj_reference,
         gex_fastq,
         gex_id,
         outdir,
+        meta_yaml,
         tempdir,
+        sample_id,
+        tcr_fastq=None,
+        tcr_id=None,
         cite_fastq=None,
         cite_id=None,
+        bcr_fastq=None,
+        bcr_id=None,
         numcores=16,
-        mempercore=10,
+        mempercore=10
 ):
-    utils.run_cellranger_multi(
+    run_cellranger_non_multiplexed(
+        reference,
+        vdj_reference,
+        gex_fastq,
+        gex_id,
+        outdir,
+        meta_yaml,
+        tempdir,
+        sample_id,
+        tcr_fastq=tcr_fastq,
+        tcr_identifier=tcr_id,
+        cite_fastq=cite_fastq,
+        cite_identifier=cite_id,
+        bcr_fastq=bcr_fastq,
+        bcr_identifier=bcr_id,
+        numcores=numcores,
+        mempercore=mempercore
+    )
+
+
+@cli.command()
+@click.option('--reference', required=True, help='CSV file path')
+@click.option('--gex_fastq', required=True, help='cores for cellranger multi')
+@click.option('--gex_id', required=True, help='cores for cellranger multi')
+@click.option('--outdir', required=True, help='cores for cellranger multi')
+@click.option('--meta_yaml', required=True, help='memory for cellranger multi')
+@click.option('--tempdir', required=True, help='cores for cellranger multi')
+@click.option('--sample_id', required=True, help='cores for cellranger multi')
+@click.option('--cite_hto_fastq', help='cores for cellranger multi')
+@click.option('--cite_hto_id', help='cores for cellranger multi')
+@click.option('--numcores', default=16, help='cores for cellranger multi')
+@click.option('--mempercore', default=10, help='cores for cellranger multi')
+def cellranger_demultiplex(
+        reference,
+        gex_fastq,
+        gex_id,
+        outdir,
+        meta_yaml,
+        tempdir,
+        sample_id,
+        cite_hto_fastq=None,
+        cite_hto_id=None,
+        numcores=16,
+        mempercore=10
+):
+    run_cellranger_demultiplex(
         reference,
         meta_yaml,
         gex_fastq,
         gex_id,
+        cite_hto_fastq,
+        cite_hto_id,
+        sample_id,
         outdir,
         tempdir,
-        cite_fastq=cite_fastq,
-        cite_identifier=cite_id,
         numcores=numcores,
-        mempercore=mempercore,
+        mempercore=mempercore
     )
 
 
@@ -52,19 +114,19 @@ def cellranger_multi(
 @click.option('--gex_fastq', required=True, help='cores for cellranger multi')
 @click.option('--gex_id', required=True, help='cores for cellranger multi')
 @click.option('--gex_metrics', required=True, help='cores for cellranger multi')
-@click.option('--tcr_fastq', help='cores for cellranger multi')
-@click.option('--tcr_id', help='cores for cellranger multi')
-@click.option('--cite_fastq', help='cores for cellranger multi')
-@click.option('--cite_id', help='cores for cellranger multi')
-@click.option('--meta_yaml', required=True, help='CSV file path')
 @click.option('--output', required=True, help='cores for cellranger multi')
+@click.option('--meta_yaml', required=True, help='CSV file path')
 @click.option('--tempdir', required=True, help='cores for cellranger multi')
 @click.option('--sample_id', required=True, help='cores for cellranger multi')
+@click.option('--tcr_fastq', help='cores for cellranger multi')
+@click.option('--tcr_id', help='cores for cellranger multi')
+@click.option('--cite_hto_fastq', help='cores for cellranger multi')
+@click.option('--cite_hto_id', help='cores for cellranger multi')
 @click.option('--bcr_fastq', help='cores for cellranger multi')
 @click.option('--bcr_id', help='cores for cellranger multi')
-@click.option('--numcores', required=True, help='cores for cellranger multi')
-@click.option('--mempercore', required=True, help='cores for cellranger multi')
-def cellranger_multi_vdj(
+@click.option('--numcores', default=16, help='cores for cellranger multi')
+@click.option('--mempercore', default=10, help='cores for cellranger multi')
+def cellranger_persample(
         reference,
         vdj_reference,
         gex_fastq,
@@ -76,14 +138,14 @@ def cellranger_multi_vdj(
         sample_id,
         tcr_fastq=None,
         tcr_id=None,
-        cite_fastq=None,
-        cite_id=None,
+        cite_hto_fastq=None,
+        cite_hto_id=None,
         bcr_fastq=None,
         bcr_id=None,
         numcores=16,
         mempercore=10,
 ):
-    utils.run_cellranger_multi_vdj(
+    run_cellranger_persample(
         reference,
         vdj_reference,
         gex_fastq,
@@ -95,8 +157,8 @@ def cellranger_multi_vdj(
         sample_id,
         tcr_fastq=tcr_fastq,
         tcr_identifier=tcr_id,
-        cite_fastq=cite_fastq,
-        cite_identifier=cite_id,
+        cite_hto_fastq=cite_hto_fastq,
+        cite_hto_identifier=cite_hto_id,
         bcr_fastq=bcr_fastq,
         bcr_identifier=bcr_id,
         numcores=numcores,
@@ -105,17 +167,49 @@ def cellranger_multi_vdj(
 
 
 @cli.command()
-@click.option('--bam_file', required=True, help='CSV file path')
-@click.option('--metrics', required=True, help='CSV file path')
+@click.option('--cellranger_demultiplex_dir', required=True, help='CSV file path')
 @click.option('--outdir', required=True, help='CSV file path')
 @click.option('--tempdir', required=True, help='CSV file path')
-def bam_to_fastq(bam_file, metrics, outdir, tempdir):
-    utils.run_bam_to_fastq(
-        bam_file,
-        metrics,
-        outdir,
-        tempdir
-    )
+def bam_to_fastq(cellranger_demultiplex_dir, outdir, tempdir):
+    run_bam_to_fastq(cellranger_demultiplex_dir, outdir, tempdir)
+
+
+@cli.command()
+@click.option('--meta_yaml', required=True, help='CSV file path')
+@click.option('--cite_hto_id', help='CSV file path')
+@click.option('--tcr_id', help='CSV file path')
+@click.option('--bcr_id', help='CSV file path')
+def check_multiplex_status(
+        meta_yaml,
+        cite_hto_id=None,
+        tcr_id=None,
+        bcr_id=None,
+):
+    metadata = yaml.safe_load(open(meta_yaml, 'rt'))
+
+    if 'meta' not in metadata:
+        print("non-multiplexed")
+        return
+
+    if metadata['meta'] is None:
+        print("non-multiplexed")
+        return
+
+    if 'hashtag' not in metadata['meta']:
+        print("non-multiplexed")
+        return
+
+    # either TCR or BCR must exist  to run all 3 steps
+    if tcr_id is None and bcr_id is None:
+        print("non-multiplexed")
+        return
+
+    # cite and hto comes in a single dir and must exist to run all 3 steps
+    if cite_hto_id is None:
+        print("non-multiplexed")
+        return
+
+    print("multiplexed")
 
 
 if __name__ == "__main__":
